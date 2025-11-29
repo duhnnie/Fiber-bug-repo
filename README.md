eca469a685b9db62ba9273bd7252876c6de70e42
Daniel Canedo <dnl.canedo@gmail.com>
2025-11-28 23:02:11 -0400

    add server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Fiber-bug-repo
-This is a repo for reproducing a fiber v2 issue
+This is a repo for reproducing a fiber v2 (2.52.10) issue
+
+## Test
+
+```
+curl http://localhost:8080/api/v1/users
+```
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func DefaultErrorHandler(c *fiber.Ctx, err error) error {
+	code := http.StatusBadRequest
+	res := map[string]any{
+		"code":         code,
+		"errorHandler": "defaultErrorHandler",
+		"message":      err.Error(),
+	}
+
+	// Send a custom error response
+	return c.Status(code).JSON(res)
+}
+
+func getUser(c *fiber.Ctx) error {
+	// It always fails
+	return errors.New("some error occurred")
+}
+
+func userRoutes() *fiber.App {
+	app := fiber.New()
+	app.Get("/", getUser)
+
+	return app
+}
+
+func V1Routes() *fiber.App {
+	app := fiber.New(fiber.Config{
+		ErrorHandler: DefaultErrorHandler,
+	})
+
+	app.Mount("/users", userRoutes())
+	return app
+}
+
+func main() {
+	port := 8080
+	app := fiber.New()
+	app.Mount("/api/v1", V1Routes())
+
+	fmt.Printf("running on port %d...\n", port)
+
+	err := app.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	// curl http://localhost:8080/api/v1/users
+}
